158d2b90cd0033a45471269bc669022f367e7a24
IvanDmitrachkov <dmitrachkovivan@gmail.com>
2018-06-13 16:31:39 +0300

    Добавьте файлы проекта.

---
+++ unclespace_ver3/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace unclespace_ver3
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ unclespace_ver3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="unclespace_ver3.MvcApplication" Language="C#" %>
